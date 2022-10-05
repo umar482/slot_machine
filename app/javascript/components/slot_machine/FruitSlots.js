259d94749abd60a5163a10ddef2033cf60263284
sadia-shahid <sadia@thesoftaims.com>
2022-10-05 15:50:11 +0500

    Add simple html of fruit slot component

--- app/javascript/components/slot_machine/FruitSlots.js
+++ app/javascript/components/slot_machine/FruitSlots.js
@@ -2,7 +2,17 @@ import React from 'react'
 
 function FruitSlots() {
   return (
-    <div>FruitSlots</div>
+    <div className='flex gap-3 w-60'>
+      <div className='bg-gray-100 h-[150px] text-center font-bold items-center flex w-full'>
+        <span className='w-full'>X</span>
+      </div>
+      <div className='bg-gray-100 h-[150px] text-center font-bold items-center flex w-full'>
+        <span className='w-full'>X</span>
+      </div>
+      <div className='bg-gray-100 h-[150px] text-center font-bold items-center flex w-full'>
+        <span className='w-full'>X</span>
+      </div>
+    </div>
   )
 }
 
