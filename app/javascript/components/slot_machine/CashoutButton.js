688ad35b4b04b94e32189d6c2a610934b033f882
sadia-shahid <sadia@thesoftaims.com>
2022-10-05 15:40:55 +0500

    Add simple html of cashout and spin button component

--- app/javascript/components/slot_machine/CashoutButton.js
+++ app/javascript/components/slot_machine/CashoutButton.js
@@ -2,7 +2,9 @@ import React from 'react'
 
 function CashoutButton() {
   return (
-    <div>CashoutButton</div>
+    <div className='bg-yellow-300 p-3 border-yellow-300 border-2 text-center text-2xl rounded cursor-pointer'>
+      Cashout
+    </div>
   )
 }
 
--- app/javascript/components/slot_machine/SpinButton.js
+++ app/javascript/components/slot_machine/SpinButton.js
@@ -2,7 +2,9 @@ import React from 'react'
 
 function SpinButton() {
   return (
-    <div>SpinButton</div>
+    <div className='bg-yellow-300 p-3 border-yellow-300 border-2 rounded text-center text-2xl cursor-pointer'>
+      Spin
+    </div>
   )
 }
 
