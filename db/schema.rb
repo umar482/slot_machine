72126e9493c8dcc4bbb81e293100872c3c7e6ccd
sadia-shahid <sadia@thesoftaims.com>
2022-10-06 09:49:50 +0500

    Update user schema

--- db/schema.rb
+++ db/schema.rb
@@ -27,6 +27,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_05_071207) do
 
   create_table "users", force: :cascade do |t|
     t.string "user_name"
+    t.integer "credit"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
